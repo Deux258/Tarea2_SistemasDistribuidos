73cc800919a581fb1bc61c4743db1727237075a6
{Pup1na} <{sebastian.zuniga@biceventures.cl}>
2025-06-30 17:49:20 -0400

    pig funcional

--- pig-filter/entrypoint.sh
+++ pig-filter/entrypoint.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+echo "esperando señal para iniciar el proceso..."
+python3 /scripts/receptor.py 
+
 echo "Exportando datos desde MongoDB..."
-mongoexport --uri="mongodb://admin:admin123@mongo:27017/waze_db?authSource=admin" --collection=eventos --out=/data/eventos.json --jsonArray
+mongoexport --uri="mongodb://admin:admin@mongo:27017/waze_db?authSource=admin" --collection=eventos --out=/data/eventos.json --jsonArray
 
 echo "Filtrando datos con Apache Pig..."
 pig -x local /filtrar_eventos.pig
 
 echo "Importando resultados filtrados a la base de datos waze_filtered..."
-mongoimport --uri="mongodb://admin:admin123@mongo:27017/waze_filtered?authSource=admin" --collection=eventos_filtrados --file=/data/eventos_filtrados --type=csv --headerline
-
-#elastic import
+mongoimport --uri="mongodb://admin:admin@mongo:27017/waze_filtered?authSource=admin" --collection=eventos_filtrados --file=/data/eventos_filtrados --type=csv --headerline
 
 echo "Proceso completado. Resultados importados en la colección eventos_filtrados de waze_filtered."
